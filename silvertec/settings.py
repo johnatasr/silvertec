66e6a60ec606149468cec88e52b439bc96b1310f
Johnatas <johnatasrs60@gmail.com>
2019-11-26 00:15:39 -0300

    heroku prob

--- silvertec/settings.py
+++ silvertec/settings.py
@@ -85,12 +85,9 @@ WSGI_APPLICATION = 'silvertec.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-
-from dj_database_url import parse as dburl
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
